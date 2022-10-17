496eab7124c36f4719883e5e3aef2715b8f01767
Wesley Adriann <wesleyadriann@gmail.com>
2022-10-17 13:09:44 -0300

    Adds grid template in home
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,18 @@
-import { Header, ContentWrapper, Input, Card } from '../../components'
+import {
+  Header,
+  ContentWrapper,
+  Input,
+  Card,
+  Button,
+} from '../../components'
 
-import { Title } from './styles'
+import {
+  Title,
+  Grid,
+  QuotaWrapper,
+  AmountWrapper,
+  TaxWrapper
+} from './styles'
 
 export const Home = () => (
   <div>
@@ -13,41 +25,68 @@ export const Home = () => (
       <Title>
         Realize uma simulação de crédito utilizando seu bem como garantia.
       </Title>
-      <Input
-        as='select'
-        label='Número de parcelas'
-        selectOptions={['24', '36', '48']}
-      />
-      <Input
-        as='select'
-        label='Garantia'
-        selectOptions={['Veículo', 'Imóvel']}
-      />
-      <Input label='Valor da Garantia'/>
-      <Input
-        as='range'
-        range={{
-          min: '0',
-          max: '100',
-          // value: '20',
-          step: '10',
-          helper: ['12.000', '24.000']
-        }}
-      />
-      <Input label='Valor do Empréstimo'/>
-      <Input
-        as='range'
-        range={{
-          min: '0',
-          max: '100',
-          // value: '20',
-          step: '10',
-          helper: ['30.000', '60.000']
-        }}
-      />
-      <Card>
-        abcs
-      </Card>
+      <Grid>
+        <Input
+          as='select'
+          gridArea='installments'
+          label='Número de parcelas'
+          selectOptions={['24', '36', '48']}
+        />
+        <Input
+          gridArea='warranty'
+          as='select'
+          label='Garantia'
+          selectOptions={['Veículo', 'Imóvel']}
+        />
+        <Input
+          gridArea='warranty-value'
+          label='Valor da Garantia'
+        />
+        <Input
+          as='range'
+          gridArea='warranty-range'
+          range={{
+            min: '0',
+            max: '100',
+            // value: '20',
+            step: '10',
+            helper: ['12.000', '24.000']
+          }}
+        />
+        <Input
+          gridArea='loan'
+          label='Valor do Empréstimo'
+        />
+        <Input
+          as='range'
+          gridArea='loan-range'
+          range={{
+            min: '0',
+            max: '100',
+            // value: '20',
+            step: '10',
+            helper: ['30.000', '60.000']
+          }}
+        />
+        <Card gridArea='card'>
+          <QuotaWrapper>
+            <h4>Valor da Parcela</h4>
+            <div>
+              <strong>R$</strong>
+              <span>465,00</span>
+            </div>
+          </QuotaWrapper>
+          <AmountWrapper>
+            <h4>Total a pagar</h4>
+            <p>R$ 11.112,00</p>
+          </AmountWrapper>
+          <TaxWrapper>
+            <h4>Taxa de juros (mês)</h4>
+            <p>111,12%</p>
+          </TaxWrapper>
+          <Button>Solicitar</Button>
+        </Card>
+      </Grid>
     </ContentWrapper>
   </div>
 )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled'
 
-
 export const Title = styled.h2`
   color: ${({ theme }) => theme.colors.secondary.gray};
   font-size: 32px;
@@ -10,3 +9,20 @@ export const Title = styled.h2`
   text-align: center;
 `
 
+export const Grid = styled.div`
+  display: grid;
+  gap: 48px;
+  grid-template-columns: repeat(2, 1fr) 1.5fr;
+  grid-template-areas:
+    "installments warranty card"
+    "warranty-value warranty-range card"
+    "loan loan-range card";
+  width: 80%;
+`
+
+
+export const QuotaWrapper = styled.div``
+
+export const AmountWrapper = styled.div``
+
+export const TaxWrapper = styled.div``
