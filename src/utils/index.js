4e129760b8c70820c70b8dc12cacedeea7d74025
Wesley Adriann <wesleyadriann@gmail.com>
2022-10-20 16:56:15 -0300

    Fixes normalize with int values and adds new utils functions
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,19 @@
 export const normalize = (value = '') =>
-  value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+  `${value}`.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+
+export const formatNumber = (value = '') =>
+  `${value}`.replace(/(\d)(?=(\d{3})+$)/g, '$1.')
+
+export const range2Value = (range, percent) => {
+  const [min, max] = range
+  const rangeValue = max - min
+  return (rangeValue * percent) + min
+}
+
+export const value2Range = (range, value) => {
+  const [min, max] = range
+  if(value <= min) return 0
+  if(value >= max) return 1
+  const rangeValue = max - min
+  return (value - min) / rangeValue
+}
