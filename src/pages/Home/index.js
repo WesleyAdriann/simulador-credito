14e69982eb4dff244ddd485cfd82811b2522f545
Wesley Adriann <wesleyadriann@gmail.com>
2022-10-20 16:56:49 -0300

    Adds to calculate range and amount inputs
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,5 @@
+import { useState, useMemo, useCallback } from 'react'
+
 import {
   Header,
   ContentWrapper,
@@ -6,6 +8,9 @@ import {
   Button,
 } from '../../components'
 
+import database from '../../infra/database.json'
+import { formatNumber, range2Value, value2Range } from '../../utils'
+
 import {
   Title,
   Grid,
@@ -13,79 +18,126 @@ import {
   CardTextWrapper
 } from './styles'
 
-export const Home = () => (
-  <div>
-    <Header />
-    <ContentWrapper
-      as='section'
-      backgroundColor='manga'
-      flexDirection='column'
-    >
-      <Title>
-        Realize uma simulação de crédito utilizando seu bem como garantia.
-      </Title>
-      <Grid>
-        <Input
-          as='select'
-          gridArea='installments'
-          label='Número de parcelas'
-          selectOptions={['24', '36', '48']}
-        />
-        <Input
-          gridArea='warranty'
-          as='select'
-          label='Garantia'
-          selectOptions={['Veículo', 'Imóvel']}
-        />
-        <Input
-          gridArea='warranty-value'
-          label='Valor da Garantia'
-        />
-        <Input
-          as='range'
-          gridArea='warranty-range'
-          range={{
-            min: '0',
-            max: '100',
-            // value: '20',
-            step: '10',
-            helper: ['12.000', '24.000']
-          }}
-        />
-        <Input
-          gridArea='loan'
-          label='Valor do Empréstimo'
-        />
-        <Input
-          as='range'
-          gridArea='loan-range'
-          range={{
-            min: '0',
-            max: '100',
-            // value: '20',
-            step: '10',
-            helper: ['30.000', '60.000']
-          }}
-        />
-        <Card gridArea='card'>
-          <QuotaWrapper>
-            <h4>Valor da Parcela</h4>
-            <div>
-              <strong>R$</strong>
-              <span>{' '}465,00</span>
-            </div>
-          </QuotaWrapper>
-          <CardTextWrapper>
-            <h4>Total a pagar</h4>
-            <p>R$ 11.112,00</p>
-          </CardTextWrapper>
-          <CardTextWrapper>
-            <h4>Taxa de juros (mês)</h4>
-            <p>111,12%</p>
-          </CardTextWrapper>
-          <Button>Solicitar</Button>
-        </Card>
-      </Grid>
-    </ContentWrapper>
-  </div>
-)
+export const Home = () => {
+  const [warranties, warrantiesId] = useMemo(() =>
+    database.warranties.reduce((acc, value) => [[...acc[0], value.name], [...acc[1], value.id]], [[], []])
+  , [])
+  const [warranty, setWarranty] = useState(database.warranties[0])
+  const [formValues, setFormValues] = useState({
+    warrantyRange: 0.5,
+    warranty: range2Value(warranty.warranty_range, 0.5),
+    loanRange: 0.5,
+    loan: range2Value(warranty.loan_range, 0.5)
+  })
+
+  const handleChangeWarrantyType = useCallback((event) => {
+    const id = event.target.value
+    setWarranty(database.warranties[id])
+  }, [])
+
+  const handleChangeRange = useCallback((type) => (event) => {
+    const floatValue = parseInt(event.target.value) / 100
+    const value = range2Value(warranty[`${type}_range`], floatValue)
+    setFormValues((prev) => ({
+      ...prev,
+      [`${type}Range`]: floatValue,
+      [type]: value
+    }))
+  }, [warranty])
+
+  const handleChangeAmount = useCallback((type) => (event) => {
+    const inputValue = event.target.value.replace(/\D/g) || '0'
+    const value = parseInt(inputValue, '')
+    const floatValue = value2Range(warranty[`${type}_range`], value)
+    setFormValues((prev) => ({
+      ...prev,
+      [`${type}Range`]: floatValue,
+      [type]: value
+    }))
+  }, [warranty])
+
+  return (
+    <div>
+      <Header />
+      <ContentWrapper
+        as='section'
+        backgroundColor='manga'
+        flexDirection='column'
+      >
+        <Title>
+          Realize uma simulação de crédito utilizando seu bem como garantia.
+        </Title>
+        <Grid>
+          <Input
+            as='select'
+            gridArea='installments'
+            label='Número de parcelas'
+            selectOptions={warranty.installments}
+          />
+          <Input
+            gridArea='warranty'
+            as='select'
+            label='Garantia'
+            onChange={handleChangeWarrantyType}
+            selectOptions={warranties}
+            selectOptionsValue={warrantiesId}
+          />
+          <Input
+            gridArea='warranty-value'
+            label='Valor da Garantia'
+            onChange={handleChangeAmount('warranty')}
+            value={formValues.warranty}
+          />
+          <Input
+            as='range'
+            gridArea='warranty-range'
+            onChange={handleChangeRange('warranty')}
+            range={{
+              min: 0,
+              max: 100,
+              value: formValues.warrantyRange * 100,
+              step: 10,
+              helper: warranty.warranty_range.map(formatNumber)
+            }}
+          />
+          <Input
+            gridArea='loan'
+            label='Valor do Empréstimo'
+            onChange={handleChangeAmount('loan')}
+            value={formValues.loan}
+          />
+          <Input
+            as='range'
+            gridArea='loan-range'
+            onChange={handleChangeRange('loan')}
+            range={{
+              min: 0,
+              max: 100,
+              value: formValues.loanRange * 100,
+              step: 10,
+              helper: warranty.loan_range.map(formatNumber)
+            }}
+          />
+          <Card gridArea='card'>
+            <QuotaWrapper>
+              <h4>Valor da Parcela</h4>
+              <div>
+                <strong>R$</strong>
+                <span>{' '}465,00</span>
+              </div>
+            </QuotaWrapper>
+            <CardTextWrapper>
+              <h4>Total a pagar</h4>
+              <p>R$ 11.112,00</p>
+            </CardTextWrapper>
+            <CardTextWrapper>
+              <h4>Taxa de juros (mês)</h4>
+              <p>111,12%</p>
+            </CardTextWrapper>
+            <Button>Solicitar</Button>
+          </Card>
+        </Grid>
+      </ContentWrapper>
+    </div>
+  )
+}
